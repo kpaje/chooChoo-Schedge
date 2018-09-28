3f2a54472a220a7129a175539185c95770881880
kpcruz <kelvinmembership@gmail.com>
2018-09-28 06:17:02 -0700

    broke moment.js while moving methods around, ended up refixing the invalid dates showing up on the frequency column

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,4 @@
+//firebase configuration
 const config = {
     apiKey: "AIzaSyAK39CFKgh3QpnZpLQ2bt-ndHxH-9tVPVk",
     authDomain: "choochoo-schedge.firebaseapp.com",
@@ -9,55 +10,59 @@ const config = {
 firebase.initializeApp(config);
 const database = firebase.database();
 
-var actions = {
-    trainName   : '',
-    destination : '', 
-    frequency   : '',
-    nextArrival : '',
+var trainName   = '';
+var destination = ''; 
+var boarding    = '';
+var nextArrival = '';
 
-    getUserInput: function() {
-        this.trainName   = $('#name-input').val().trim();
-        this.destination = $('#destination-input').val().trim();
-        this.frequency   = $('#frequency-input').val().trim();
-        this.nextArrival = $('#nextArrival-input').val().trim();
-    },
+var capture = {
     uploadData: function() {
         return database.ref().push({
-            name:        this.trainName,
-            destination: this.destination,
-            frequency:   this.frequency,
-            arrival: this.nextArrival
+            name        : trainName,
+            destination : destination,
+            boarding    : boarding,
+            arrival     : nextArrival
         });
     },
+    userInputs: function() {
+        trainName   = $('#name-input').val().trim();
+        destination = $('#destination-input').val().trim();
+        boarding    = $('#boarding-input').val().trim();
+        nextArrival = $('#nextArrival-input').val().trim();
+        return;
+    },
+    childSnapShots: function(childSnapshot) {
+        trainName   = childSnapshot.val().name;
+        destination = childSnapshot.val().destination;
+        boarding    = childSnapshot.val().boarding;
+        nextArrival = childSnapshot.val().arrival;
+        return;
+    },
+};
+
+var actions = {
     clearTextBoxes: function() {
         $('#name-input').val('');
         $('#destination-input').val('');
-        $('#frequency-input').val('');
+        $('#boarding-input').val('');
         $('#nextArrival-input').val('');
-    },
-    storeChildSnapShot: function(childSnapshot) {
-        this.trainName   = childSnapshot.val().name;
-        this.destination = childSnapshot.val().destination;
-        this.frequency   = childSnapshot.val().frequency;
-        this.nextArrival = childSnapshot.val().arrival;
-        return;
     },
     convertTime: function(time) {
         return moment(time, 'hh:mm a').format('HH:mm');
     },
     calculateMinutesAway: function() {
-        var now  = moment.utc(this.frequency, "HH:mm");
-        var then = moment.utc(this.nextArrival, "HH:mm");
-        if (then.isBefore(now)) then.add(1, 'day');
+        var now  = moment.utc(boarding, "HH:mm");
+        var then = moment.utc(nextArrival, "HH:mm");
         var eta  =  moment.duration(then.diff(now));
+        if (then.isBefore(now))then.add(1, 'day');
         return moment.duration(eta).as('minutes');
     },
-    createRowData: function() {
+    populateRowData: function() {
         var tableData = $('<tr>').append(
-            $('<td>').text(this.trainName),
-            $('<td>').text(this.destination),
-            $('<td>').text(this.convertTime(this.frequency)),
-            $('<td>').text(this.convertTime(this.nextArrival)),
+            $('<td>').text(trainName),
+            $('<td>').text(destination),
+            $('<td>').text(this.convertTime(boarding)),
+            $('<td>').text(this.convertTime(nextArrival)),
             $('<td>').text(this.calculateMinutesAway()),
         );
         $('#train-table > tbody').append(tableData);
@@ -67,21 +72,18 @@ var actions = {
 var handlers = {
     getUserInput: $('#add-train-btn').on('click', function(event) {
         event.preventDefault();
-        actions.getUserInput();
-        actions.uploadData();
+        capture.userInputs();
+        capture.uploadData();
         console.log('train successfully added');
         actions.clearTextBoxes();
     }),
-    
     logEntry: database.ref().on('child_added', function(childSnapshot) {
-        actions.storeChildSnapShot(childSnapshot);
-        actions.createRowData();
+        capture.childSnapShots(childSnapshot);
+        actions.populateRowData();
     }),
-
     scrollToAccordianContent: $('.collapse').on('shown.bs.collapse', function() {
         $('html,body').animate({
             scrollTop: $('.card-body').offset().top
         });
     }),
 };
-
